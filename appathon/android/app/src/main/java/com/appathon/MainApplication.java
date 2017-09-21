494d16cbf966b00285c99389a07cfee81308dfb9
yash <yash.aocorp@gmail.com>
2017-09-21 09:29:50 +0530

    using rn vector icons

--- appathon/android/app/src/main/java/com/appathon/MainApplication.java
+++ appathon/android/app/src/main/java/com/appathon/MainApplication.java
@@ -3,6 +3,7 @@ package com.appathon;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import io.realm.react.RealmReactPackage;
 import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new LinearGradientPackage(),
             new RealmReactPackage(),
             new GoogleAnalyticsBridgePackage()
--- appathon/app/assets/icons.js
+++ appathon/app/assets/icons.js
@@ -1,5 +1,9 @@
+import React from 'react'
+import Octicon from 'react-native-vector-icons/Octicons'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
 export const FontIcons = {
-    login: '',
+    login: <Octicon name="lock" size={30} color="#900" />,
     navigation: '',
     article: '',
     profile: '',
@@ -16,15 +20,15 @@ export const FontIcons = {
     heart: String.fromCharCode(61444),
     comment: String.fromCharCode(61669),
     user: String.fromCharCode(62144),
-    twitter: String.fromCharCode(61593),
-    google: String.fromCharCode(61856),
-    facebook: String.fromCharCode(61594),
+    twitter: <Icon name="twitter" size={30} color="#900" />,
+    google: <Icon name="google" size={30} color="#900" />,
+    facebook: <Icon name="facebook" size={30} color="#900" />,
     plus: String.fromCharCode(61543),
     search: String.fromCharCode(61442),
     smile: String.fromCharCode(61720),
-    chevronRight: String.fromCharCode(61524),
-    chevronLeft: String.fromCharCode(61700),
-    bars: String.fromCharCode(61641),
+    chevronRight: <Icon name="chevron-right" size={30} color="#000" />,
+    chevronLeft: <Icon name="chevron-left" size={30} color="#000" />,
+    bars: <Icon name="bars" size={30} color="#000" />,
     slashEye: String.fromCharCode(61552),
     github: String.fromCharCode(61595),
   };
